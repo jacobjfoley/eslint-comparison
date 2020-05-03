54e1aadc6989e829df5f679a7c76307f1be545e0
Jacob Foley <jacob.j.foley@gmail.com>
2020-05-04 07:11:22 +1000

    Switch ruleset ordering

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,18 +3,18 @@ import rules from "../../data";
 import "./App.css";
 
 function App() {
-  const header = ["Rule", "Eslint", "React", "Airbnb"].map((text) => (
+  const header = ["Rule", "React", "Eslint", "Airbnb"].map((text) => (
     <div key={text}>{text}</div>
   ));
 
   const cells = rules.ruleset.reduce((cells, rule) => {
-    const eslintRule = rules.eslintRules[rule]?.level || "-";
     const reactRule = rules.reactRules[rule]?.level || "-";
+    const eslintRule = rules.eslintRules[rule]?.level || "-";
     const airbnbRule = rules.airbnbRules[rule]?.level || "-";
 
     cells.push(<div key={rule}>{rule}</div>);
-    cells.push(<div key={`${rule}-eslint`}>{eslintRule}</div>);
     cells.push(<div key={`${rule}-react`}>{reactRule}</div>);
+    cells.push(<div key={`${rule}-eslint`}>{eslintRule}</div>);
     cells.push(<div key={`${rule}-airbnb`}>{airbnbRule}</div>);
 
     return cells;
