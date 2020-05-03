d858b7a76ac68aa6b336eefe17659b6bac056b21
Jacob Foley <jacob.j.foley@gmail.com>
2020-05-03 16:12:19 +1000

    Initial output code

--- scripts/prepareData.sh
+++ scripts/prepareData.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Extract configuration for each eslint config.
 (cd ./src/configs/airbnb && npx eslint --print-config .eslintrc.js > ../../data/airbnb.json)
 (cd ./src/configs/eslint && npx eslint --print-config .eslintrc.js > ../../data/eslint.json)
 (cd ./src/configs/react && npx eslint --print-config .eslintrc.js > ../../data/react.json)
