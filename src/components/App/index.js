654c4fd82464887e0d99b0aae3f9215735bb6020
Jacob Foley <jacob.j.foley@gmail.com>
2020-05-04 07:03:19 +1000

    Render table

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,22 +1,31 @@
 import React from "react";
+import rules from "../../data";
 import "./App.css";
 
 function App() {
+  const header = ["Rule", "Eslint", "React", "Airbnb"].map((text) => (
+    <div key={text}>{text}</div>
+  ));
+
+  const cells = rules.ruleset.reduce((cells, rule) => {
+    const eslintRule = rules.eslintRules[rule] || "-";
+    const reactRule = rules.reactRules[rule] || "-";
+    const airbnbRule = rules.airbnbRules[rule] || "-";
+
+    cells.push(<div key={rule}>{rule}</div>);
+    cells.push(<div key={`${rule}-eslint`}>{eslintRule.level}</div>);
+    cells.push(<div key={`${rule}-react`}>{reactRule.level}</div>);
+    cells.push(<div key={`${rule}-airbnb`}>{airbnbRule.level}</div>);
+
+    return cells;
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="grid">
+        {header}
+        {cells}
+      </div>
     </div>
   );
 }
